adf34c5c58aab6ddf98abc2a392c52f2e502e698
Vikas Dev Pandey <vikaspandey0234@gmail.com>
2024-07-08 11:18:39 +0530

    initial stage 0

---
+++ script.js
@@ -0,0 +1,41 @@
+function loading(){
+    var tl = gsap.timeline()
+
+tl.to("#yellow1",{
+    top:"-100%",
+    delay:0.5,
+    duration: 0.7,
+    ease:"expo.out"
+})
+tl.from("#yellow2",{
+    top:"100%",
+    delay:0.5,
+    duration: 0.7,
+    ease:"expo.out"
+},"anim")
+tl.to("#loader h1",{
+    delay:0.2,
+    color:"black"
+},"anim")
+tl.to("#loader",{
+    opacity:0
+})
+tl.to("#loader",{
+    display:"none"
+}) 
+}
+loading()
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var elems = document.querySelectorAll("#elem");
+var main = document.querySelector("#page2");
+
+elems.forEach(function(ele) {
+    ele.addEventListener("mouseenter", function() {
+        var bgimg = ele.getAttribute("data-img");
+        main.style.backgroundImage = `url(${bgimg})`;
+    });
+});
